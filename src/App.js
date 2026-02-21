b73cf47be82e7187761c61b46ac2d021d6d1356f
wsuits6 <wsuits6@gmail.com>
2026-02-21 02:28:10 +0000

    aded pizza  component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+function Pizza() {
+  return (
+    <div className="pizza">
+      <img src="https://www.simplyscratch.com/wp-content/uploads/2023/04/Garlic-Herb-Olive-Oil-Bread-Dip-l-SimplyScratch-13-700x1049.jpg" /> 
+      <div>
+        <h3>Focaccia</h3>
+        <p>Bread with Italian Olive Oil</p>
+        <span>$6</span>
+      </div>
+    </div>
+  )
+}
+
+
 function App() {
   return (
     <div className="App">
       <h1>Hello React</h1>
       <p>Lets build an aweosme Pizza menu together</p>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+
     </div>
   );
 };
