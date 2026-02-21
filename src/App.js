ea159df322b9476d9f385848f52f729c5a83471c
wsuits6 <wsuits6@gmail.com>
2026-02-21 02:39:38 +0000

    adde foooter component

--- src/App.js
+++ src/App.js
@@ -11,6 +11,25 @@ function Pizza() {
   )
 }
 
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  
+  if (hour >= openHour && hour <= closeHour) {
+    alert("We're currently open!");
+  } else {
+    alert("Sorry, we're closed");
+  }
+  
+  return (
+    <footer>
+      We're currently open. {new Date().toLocaleTimeString()}
+    </footer>
+  );
+}
+
+
 
 function App() {
   return (
@@ -21,6 +40,7 @@ function App() {
       <Pizza />
       <Pizza />
 
+      <Footer />
     </div>
   );
 };
